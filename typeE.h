3c2bead0d7d6a47e5b82611edf84fb7ac911476a
srcunningham <scunnin3@fitz95.helios.nd.edu>
2015-04-08 13:41:19 -0400

    brick.h added

---
+++ typeE.h
@@ -0,0 +1,19 @@
+//typeE.h
+//typeE is an empty brick 
+
+#ifndef TYPEE_H
+#define TYPEE_H
+
+#include <iostream>
+
+class typeE{
+	
+	public:
+		typeE();
+	
+	private:
+};
+
+
+#endif
+
---
+++ typeP.h
@@ -0,0 +1,18 @@
+//typeP.h
+//typeP is the paddle brick 
+#ifndef TYPEP_H
+#define TYPEP_H
+
+#include <iostream>
+
+class typeP{
+	
+	public:
+		typeP();
+	
+	private:
+};
+
+
+#endif
+
